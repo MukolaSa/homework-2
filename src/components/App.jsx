1562ab9f6eeb15a2bfad570b52994617cd800a7e
Mukola <koliunia2006@gmail.com>
2023-08-17 20:04:35 +0300

    ended work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from './Profile';
+import user from '../user.json';
+
+export function App() {
+    return (
+        <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    )
+}
