346174a8324d636bdca7327a9aa56f3d4118acf3
Mukola <koliunia2006@gmail.com>
2023-08-17 21:10:08 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
-import { Profile } from './Profile';
+// Profile
+import { Profile } from './Profile/Profile';
 import user from '../user.json';
+// Transactions
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
 
 export function App() {
-    return (
-        <Profile
+  return (
+    <>
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
-    )
+      <TransactionHistory items={transactions} />
+    </>
+  );
 }
